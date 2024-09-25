dde6a71f3c0faa547bcb04e3cc992a16bfedb864
Ricky-T-141 <riley.tangen@gmail.com>
2024-09-25 09:31:32 -0500

    Animal Survey Plus Notes

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,15 +1,8 @@
-user_input1 = input("what is your favorite animal?\n-")
-print(user_input1)
-user_input2 = input("Why do you like them?\n-")
-print(user_input2)
-user_input3 = input("What is a fun fact about them?\n-")
-print(user_input3)
-user_input4 = input("What color are they?\n-")
-print(user_input4)
-user_input5 = input("Where do they live?\n-")
-print(user_input5)
-user_input6 = input("What type of animal are they (Land/Aquatic, Mammle/Reptile)?\n-")
-print(user_input6)
-user_input7 = input("If you have one/could have one, what is its name/what would you name it?\n-")
-print(user_input7)
-print("Your favorite animal is the " + user_input1 + " because " + user_input2 + ". " + "Something cool about them is that " + user_input3 + ". " + "The color they are is " + user_input4 + ". " + "They live in " + user_input5 + ". " + "They are a" + user_input6 + ". " + "Your ideal name for it is " + user_input7 + ".")
+user_input1 = input("what is your favorite animal?\n-")                                     #Ask user what their favorite animal is 
+user_input2 = input("Why do you like them?\n-")                                             #Ask user why they like them
+user_input3 = input("What is a fun fact about them?\n-")                                    #Ask user for a fact about that animal
+user_input4 = input("What color are they?\n-")                                              #Ask user what color their favorite animal is
+user_input5 = input("Where do they live?\n-")                                               #Ask user where their favorite animal lives
+user_input6 = input("What type of animal are they (Land/Aquatic, Mammle/Reptile)?\n-")      #Ask user what type of animal it is
+user_input7 = input("If you have one/could have one, what is its name/what would you name it?\n-")  #Ask user what they would name their animal
+print("Your favorite animal is the " + user_input1 + " because " + user_input2 + ". " + "Something cool about them is that " + user_input3 + ". " + "The color they are is " + user_input4 + ". " + "They live in " + user_input5 + ". " + "They are a" + user_input6 + ". " + "Your ideal name for it is " + user_input7 + ".") #Sums up all the facts about users favorite animal
