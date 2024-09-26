76e6a47cb968e57c10a3524f58cd294314f13f99
Ricky-T-141 <riley.tangen@gmail.com>
2024-09-26 09:22:36 -0500

    Simple Mad Lib

---
+++ Assignments/simplemadlib.py
@@ -0,0 +1,8 @@
+temerature = input("Name an extreme temperature\n-")                #One day during a very (temerature) day,              #There was a (weird animal), wandering towards a (noun)
+Weird_Animal = input("Name a very weird/uniqe animal\n-")                #when the (weird animal) reached the (noun), it was spotted by a (adgective)(noun)
+Noun = input("Type in a noun\n-")                #The (adjective)(noun) proceded to (verb) absoulutly everywhere
+Adjective = input("Type in an adjective\n-")                #So much so that it started to make the (weird animal) (emotion)
+Noun2 = input("Type in a new noun\n-")
+Verb = input("Insert extreme verb\n-")
+Emotion = input("Name an emotion\n-")
+print("One day during a very " + temerature + " day, there was a " + Weird_Animal + ", wandering towards a " + Noun + ". When the " + Weird_Animal + " reached the " + Noun + ", it was spotted by a " + Adjective + " " + Noun2 + ". The " + Adjective + " " + Noun2 + " proceded to " + Verb + " absoulutly everywhere. So much so that it started to make the " + Weird_Animal + " " + Emotion + ".")
